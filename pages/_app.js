34633912996e205f4f7707097bd7b3f1758275b8
Miguel Martinez <miguel.emezero@gmail.com>
2021-12-22 15:01:30 -0600

    first version to deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
+import "../styles/prism-night-owl.css";
 
 import theme, { ThemeProvider } from "../styles/theme";
-
 
 function MyApp({ Component, pageProps }) {
   return <ThemeProvider>    
