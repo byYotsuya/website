7108de25637a50a2a8713a03b48b1c400cce0391
Miguel Martinez <miguel.emezero@gmail.com>
2022-06-07 19:37:10 -0500

    Minor changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,6 @@
 
-export default function Layout({ children, metadata = {}, type = "list" }) {
-
-  const isBlogTemplate = type == "list" && metadata.date
-
+export default function Layout ({ children, metadata = {}, type = 'list' }) {
+  const isBlogTemplate = type === 'list' && metadata.date
 
   return (
     <div className='h-full p-2 md:p-3 lg:p-5 xl:p-7 rounded-sm w-full lg:max-w-2xl'>
@@ -15,17 +13,16 @@ export default function Layout({ children, metadata = {}, type = "list" }) {
               <h1 className='text-4xl m-2 dark:text-gray-400 '>
                 {metadata.title}
               </h1>
-            )
+              )
             : (
               <h2 className='text-4xl font-bold dark:text-gray-400'>
                 <strong>{metadata.title}</strong>
               </h2>
-            )
+              )
         }
-        <section style={{ marginTop: "2rem" }} >{children}</section>
+        <section style={{ marginTop: '2rem' }}>{children}</section>
 
       </div>
-
 
     </div>
   )
