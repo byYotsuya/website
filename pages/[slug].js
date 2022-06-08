c5dd9f6e81b594928e7cbdcd1842c5632fe5a600
Miguel Martinez <miguel.emezero@gmail.com>
2022-06-07 19:22:20 -0500

    some changes

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,16 +1,16 @@
-import { MDXRemote } from 'next-mdx-remote';
-import Head from 'next/head';
+import { MDXRemote } from 'next-mdx-remote'
+import Head from 'next/head'
 import React from 'react'
-import AboutCard from '../components/cards/AboutCard';
-import Header from '../components/Header';
+import AboutCard from '../components/cards/AboutCard'
+import Header from '../components/Header'
 import Layout from '../components/Layout'
-import { MDXComponents } from '../components/MDXComponents';
+import { MDXComponents } from '../components/MDXComponents'
 import { getFileBySlug, getFiles } from '../lib/md'
 
+// wallerman
+// marine hololive
 
-
-export default function blog({ source, frontmatter }) {
-
+export default function blog ({ source, frontmatter }) {
   return (
     <>
 
@@ -18,7 +18,7 @@ export default function blog({ source, frontmatter }) {
         <title>{frontmatter.title}</title>
       </Head>
 
-      <Header background="#f1f5f9" />
+      <Header background='#f1f5f9' />
 
       <div className='max-w-2xl px-5 md:px-10 mt-2 h-auto'>
         <div className='text-xl flex-col md:flex-row items-center justify-between'>
@@ -35,31 +35,30 @@ export default function blog({ source, frontmatter }) {
   )
 }
 
-export async function getStaticPaths() {
-  const posts = await getFiles("posts");
+export async function getStaticPaths () {
+  const posts = await getFiles('posts')
   const paths = posts.map((post) => ({
     params: {
-      slug: post.replace(/\.mdx/, ""),
-    },
-  }));
+      slug: post.replace(/\.mdx/, '')
+    }
+  }))
 
   return {
     paths,
-    fallback: false,
-  };
+    fallback: false
+  }
 }
 
-
-export async function getStaticProps({ params }) {
-  const { source, frontmatter } = await getFileBySlug("posts", params.slug);
+export async function getStaticProps ({ params }) {
+  const { source, frontmatter } = await getFileBySlug('posts', params.slug)
 
   return {
     props: {
       source,
       frontmatter: {
         slug: params.slug,
-        ...frontmatter,
-      },
-    },
-  };
+        ...frontmatter
+      }
+    }
+  }
 }
