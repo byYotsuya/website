ebd7f84a077fe95d67c0a092857003a43857c16f
Miguel Martinez <miguel.emezero@gmail.com>
2022-06-07 19:36:36 -0500

    remove semi colon

--- components/CustomImage.js
+++ components/CustomImage.js
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 export const CustomImage = (props) => {
   return (
-    <div className="shadow-lg border-1 border-red-500 p-6 rounded-md bg-white">
+    <div className='shadow-lg border-1 border-red-500 p-6 rounded-md bg-white'>
       <Image {...props} alt={props.src} />
     </div>
-  );
-};
+  )
+}
